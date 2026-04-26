88304d211d03c993ea9aa095770682dad8879d67
Kehinde Olalusi <kehindeolalusi75@gmail.com>
2026-04-26 18:07:23 +0100

    Add feature-target EDA and update tests

--- src/ssdf/features/feature.py
+++ src/ssdf/features/feature.py
@@ -9,7 +9,17 @@ def create_target():
     target = prep_mlforecast_data(target).drop(["store_nbr", "family"], axis=1)
     target.to_parquet(FEATURES_DATA_DIR / "target.parquet")
     print("Successfully created target variable")
-
+def create_features():
+    print("Creating features...")
+    target = pd.read_parquet(FEATURES_DATA_DIR / "target.parquet")
+    promotions = pd.read_parquet(PROCESSED_DATA_DIR / "promotions.parquet")
+    
+    features = prep_mlforecast_data(promotions)
+    features = features.merge(target[['date', 'unique_id']], on=['date', 'unique_id'], how='inner')
+    
+    features.to_parquet(FEATURES_DATA_DIR / "features.parquet")
+    print("Successfully created features")
 
 if __name__ == "__main__":
     create_target()
+    create_features()
